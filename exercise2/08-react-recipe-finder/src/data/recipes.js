2f4c44e05e01630af239212849f7450c751dadd1
brentrwill <brentrwill@gmail.com>
2026-04-23 16:13:49 -0600

    removing files.

--- exercise2/08-react-recipe-finder/src/data/recipes.js
+++
@@ -1,6 +0,0 @@
-export const recipes = [
-  { id: 1, name: 'Veggie Pasta', category: 'Dinner', time: '30 min', ingredients: ['pasta','zucchini','tomato','garlic'], steps: ['Boil pasta','Cook vegetables','Combine and serve'] },
-  { id: 2, name: 'Berry Smoothie', category: 'Breakfast', time: '10 min', ingredients: ['berries','banana','yogurt','honey'], steps: ['Add ingredients to blender','Blend until smooth','Serve cold'] },
-  { id: 3, name: 'Chicken Tacos', category: 'Dinner', time: '25 min', ingredients: ['chicken','tortillas','lettuce','salsa'], steps: ['Cook chicken','Warm tortillas','Assemble tacos'] },
-  { id: 4, name: 'Greek Salad', category: 'Lunch', time: '15 min', ingredients: ['cucumber','tomato','feta','olives'], steps: ['Chop vegetables','Add feta and olives','Toss with dressing'] }
-];
